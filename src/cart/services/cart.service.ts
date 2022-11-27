3f7d6df6ea8e02e3011efc9301de1da36488d58b
HannaHerman <mrs.hanna.herman@gmail.com>
2022-11-27 17:40:34 +0100

    feat: add cart db integration

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Client } from 'pg';
 
 import { v4 } from 'uuid';
 
 import { Cart } from '../models';
 
+const CART_ID = "1eb136f4-dda4-4b89-9b27-0ae548237b84"; // TODO: remove stub and add auth
+
+
 @Injectable()
 export class CartService {
   private userCarts: Record<string, Cart> = {};
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  async findByUserId(userId: string): Promise<Cart> {
+    console.log("init client");
+    const dbClient = await this.getClient();
+    console.log("client is created");
+
+    const itemsQuery = {
+      text: `SELECT * FROM cart_items WHERE cart_id = $1`,
+      values: [CART_ID],
+    };
+
+    const items = await dbClient.query(itemsQuery);
+    console.log("search is finished");
+    return {
+      id: CART_ID,
+      items: items?.rows,
+    };
   }
 
   createByUserId(userId: string) {
@@ -19,13 +37,13 @@ export class CartService {
       items: [],
     };
 
-    this.userCarts[ userId ] = userCart;
+    this.userCarts[userId] = userCart;
 
     return userCart;
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const userCart = await this.findByUserId(userId);
 
     if (userCart) {
       return userCart;
@@ -34,22 +52,59 @@ export class CartService {
     return this.createByUserId(userId);
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, body: any): Promise<Cart> {
+    const dbClient = await this.getClient();
+    const { product } = body;
+    const findQuery = {
+      text: 'SELECT * FROM cart_items WHERE cart_id = $1 AND product_id = $2',
+      values: [CART_ID, body?.product?.product_id],
+    };
 
-    const updatedCart = {
-      id,
-      ...rest,
-      items: [ ...items ],
+    const item = await dbClient.query(findQuery);
+
+    const updateQuery = {
+      text: "",
+      values: [CART_ID, product?.product_id, product?.count],
+    };
+
+    if (item.rows.length) {
+      updateQuery.text = `UPDATE cart_items
+      SET count = $3
+      WHERE cart_id = $1 AND product_id = $2
+      RETURNING *`;
+    } else {
+      updateQuery.text = `insert into cart_items (cart_id, product_id, count) values
+      ($1, $2, $3)`
     }
 
-    this.userCarts[ userId ] = { ...updatedCart };
+    const updatedCart = await dbClient.query(updateQuery);
 
-    return { ...updatedCart };
+    return {
+      id: CART_ID,
+      items: updatedCart.rows,
+    };
   }
 
   removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+    this.userCarts[userId] = null;
   }
 
+  async getClient() {
+    const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+    const dbOptions = {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USERNAME,
+      password: PG_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      connectionTimeoutMillis: 10000,
+    };
+    const client = new Client(dbOptions);
+    await client.connect();
+
+    return client;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
+
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 let server: Handler;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: (req, callback) => callback(null, true),
   });
+  app.use(helmet());
+
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
+
   return serverlessExpress({ app: expressApp });
 }
 
